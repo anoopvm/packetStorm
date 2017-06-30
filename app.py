abc4e0a7465b850215f0be53ebb96952b91b41fb
Swathin <swathins079@gmail.com>
2017-06-30 23:37:12 +0530

    added dialog to prompt for user inputs

--- app.py
+++ app.py
@@ -2,19 +2,28 @@
 from lib.tcp import Tcp
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
-@app.route("/protocols/tcp")
+@app.route("/generate_packets", methods = ['POST'])
 def api_proto_tcp():
-	gen = Tcp("127.0.0.1","127.0.0.1",1000,1000,1,"helloooo!!!")
-	#gen.generate(int(sys.argv[1]))
-	gen.generate(10)
-	return render_template('index.html')
+    src_ip = request.form['src-ip']
+    dst_ip = request.form['dst-ip']
+    src_port = int(request.form['src-port'])
+    dst_port = int(request.form['dst-port'])
+    #proto_number = int(request.form['proto-number'])
+    no_of_packets = int(request.form['no-of-packets'])
+    payload = request.form['payload']
+
+    #gen = Tcp("127.0.0.1","127.0.0.1",1000,1000,1,"helloooo!!!")
+    gen = Tcp(src_ip,dst_ip,src_port,dst_port,1,str(payload))
+    #gen.generate(int(sys.argv[1]))
+    gen.generate(no_of_packets)
+    return render_template('index.html')
 
 #if __name__ == "__main__":
 app.run()
